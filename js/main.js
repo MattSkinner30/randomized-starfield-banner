561c5a9928dcaafee37a84810e7984c00b3315ec
MattSkinner30 <MattSkinner@Matts-MacBook-Pro.local>
2016-04-05 14:01:02 -0400

    indentation

--- js/main.js
+++ js/main.js
@@ -7,10 +7,10 @@ for (i = 0; i < 75; i++) {
   $star = $('<div>');
   $star.addClass('star');
   $star.css({
-	top: Math.random() * height,
-	left: Math.random() * width,
-	opacity: Math.random(),
-	transform: 'rotate(' + Math.random() * 360 + 'deg) scale(' + Math.random() + ')'
+    top: Math.random() * height,
+    left: Math.random() * width,
+    opacity: Math.random(),
+    transform: 'rotate(' + Math.random() * 360 + 'deg) scale(' + Math.random() + ')'
   });
   $body.append($star);
 }
